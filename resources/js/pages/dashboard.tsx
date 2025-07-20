b737f2a414d0d8c055e2aaaec5c233c113681fe9
Anang Firmansyah <>
2025-07-20 16:18:33 +0800

    setup sidebar

---
+++ resources/js/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from '@/layouts/dashboard-layout';
+
+export default function Page() {
+  return (
+    <DashboardLayout>
+      <div>
+        <h1>Ini dashboard</h1>
+      </div>
+    </DashboardLayout>
+  );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return inertia('home');
 })->name('home');
+
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', DashboardController::class)->name('dashboard');
+
+});
